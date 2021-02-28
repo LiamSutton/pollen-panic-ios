f7484cfa1c9f689803cfb9a5083e4c4ff74418ec
Liam Sutton <liamsutton1997@outlook.com>
2021-02-28 18:13:26 +0000

    changed title bar colour to yellow

--- PollenPanic/MainMenuViewController.swift
+++ PollenPanic/MainMenuViewController.swift
@@ -10,5 +10,18 @@ import UIKit
 class MainMenuViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        UIWindow.appearance()
+    }
+    
+    // Perma change navigation bar colour to yellow and text to white
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let appearance = UINavigationBarAppearance()
+        
+        appearance.backgroundColor = .systemYellow
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.tintColor = .white
     }
 }
