a3d4722677614af3448c26950045bd658678ef5c
Liam Sutton <liamsutton1997@outlook.com>
2021-03-01 15:13:05 +0000

    created game over view and linked it to game and main menu views

---
+++ PollenPanic/GameOverViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GameOverViewController.swift
+//  PollenPanic
+//
+//  Created by Liam Sutton on 01/03/2021.
+//
+
+import UIKit
+
+class GameOverViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
+    }
+}
--- PollenPanic/MainMenuViewController.swift
+++ PollenPanic/MainMenuViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class MainMenuViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
+        
         UIWindow.appearance()
     }
     
