314351da90374522b9e62363556555ab520d23ef
Liam Sutton <liamsutton1997@outlook.com>
2021-03-03 09:52:46 +0000

    Added Constants file, bee facts now pull from this

---
+++ PollenPanic/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  PollenPanic
+//
+//  Created by Liam Sutton on 03/03/2021.
+//
+
+class Constants {
+    static let BEE_FACTS:[String] =
+        [
+            "All worker bees are female",
+            "A bee produces a teaspoon of honey in her lifetime",
+            "The type of flower bees take their nectar from determienes the honeys flavour",
+            "Bees dont want to sting you as it will kill them"
+        ]
+}
--- PollenPanic/GameOverViewController.swift
+++ PollenPanic/GameOverViewController.swift
@@ -8,12 +8,11 @@
 import UIKit
 
 class GameOverViewController : UIViewController {
-    let beeFacts:[String] = ["Fact 1", "Fact 2", "Fact 3", "Fact 4"]
     @IBOutlet weak var beeFactLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.hidesBackButton = true
         
-        beeFactLabel.text = "Did you know: " + beeFacts.randomElement()! + "?"
+        beeFactLabel.text = Constants.BEE_FACTS.randomElement()!
     }
 }
