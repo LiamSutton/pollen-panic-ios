a75a4b5e85406950b95c70e1fc36c7e780ed9806
Liam Sutton <liamsutton1997@outlook.com>
2021-03-03 09:10:07 +0000

    Bee fact works with dummy data

--- PollenPanic/GameOverViewController.swift
+++ PollenPanic/GameOverViewController.swift
@@ -8,8 +8,12 @@
 import UIKit
 
 class GameOverViewController : UIViewController {
+    let beeFacts:[String] = ["Fact 1", "Fact 2", "Fact 3", "Fact 4"]
+    @IBOutlet weak var beeFactLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.hidesBackButton = true
+        
+        beeFactLabel.text = "Did you know: " + beeFacts.randomElement()! + "?"
     }
 }
