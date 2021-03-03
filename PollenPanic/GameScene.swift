c071f2da9c728b9f0163a6c3d5e751d02f60d274
Liam Sutton <liamsutton1997@outlook.com>
2021-03-03 14:54:35 +0000

    Bee now moves from gesture input

--- PollenPanic/GameScene.swift
+++ PollenPanic/GameScene.swift
@@ -6,9 +6,51 @@
 //
 
 import SpriteKit
-
 class GameScene : SKScene {
+    let player = SKSpriteNode(imageNamed: "bee.png")
+    var currentDir:CGFloat = 0.0
     override func didMove(to view: SKView) {
         
+        let leftSwipeGestureHandler = UISwipeGestureRecognizer(
+            target: self, action: #selector(GameScene.handleLeftSwipeGesture(sender:))
+        )
+        
+        let rightSwipeGestureHandler = UISwipeGestureRecognizer(
+            target: self, action: #selector(GameScene.handleRightSwipeGesture(sender:))
+        )
+        
+        let downSwipeGestureHandler = UISwipeGestureRecognizer(
+            target: self, action: #selector(GameScene.handleDownSwipeGesture(sender:))
+        )
+        
+        leftSwipeGestureHandler.direction = .left
+        rightSwipeGestureHandler.direction = .right
+        downSwipeGestureHandler.direction = .down
+        
+        view.addGestureRecognizer(leftSwipeGestureHandler)
+        view.addGestureRecognizer(rightSwipeGestureHandler)
+        view.addGestureRecognizer(downSwipeGestureHandler)
+        
+        backgroundColor = SKColor.systemTeal
+        player.size = CGSize(width: 64, height: 64)
+        player.position = CGPoint(x:size.width * 0.5, y: size.height * 0.1)
+        
+        addChild(player)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        player.position.x += currentDir
+    }
+    
+    @objc func handleLeftSwipeGesture (sender: UISwipeGestureRecognizer) {
+        currentDir = -2.0
+    }
+    
+    @objc func handleRightSwipeGesture (sender: UISwipeGestureRecognizer) {
+        currentDir = 2.0
+    }
+    
+    @objc func handleDownSwipeGesture (sender: UISwipeGestureRecognizer) {
+        currentDir = 0.0
     }
 }
--- PollenPanic/GameViewController.swift
+++ PollenPanic/GameViewController.swift
@@ -17,52 +17,13 @@ class GameViewController : UIViewController {
         super.viewDidLoad()
         self.navigationItem.hidesBackButton = true
         
-        let leftSwipeGestureHandler = UISwipeGestureRecognizer(
-            target: self, action: #selector(handleLeftSwipeGesture(sender:))
-        )
-        
-        let rightSwipeGestureHandler = UISwipeGestureRecognizer(
-            target: self, action: #selector(handleRightSwipeGesture(sender:))
-        )
-        
-        let downSwipeGestureHandler = UISwipeGestureRecognizer(
-            target: self, action: #selector(handleDownSwipeGesture(sender:))
-        )
-        
-        leftSwipeGestureHandler.direction = .left
-        rightSwipeGestureHandler.direction = .right
-        downSwipeGestureHandler.direction = .down
-        
-        self.view.addGestureRecognizer(leftSwipeGestureHandler)
-        self.view.addGestureRecognizer(rightSwipeGestureHandler)
-        self.view.addGestureRecognizer(downSwipeGestureHandler)
-        
-        if let view = self.view as? SKView {
-            let scene = SKScene(size: view.bounds.size)
-            scene.scaleMode = .resizeFill
-            scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-            let image = SKSpriteNode(imageNamed: "bee.png")
-            image.size = CGSize(width: 64, height: 64)
-            
-            scene.addChild(image)
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-            
-            view.presentScene(scene)
-        }
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
         
         
-    }
-    
-    @objc func handleLeftSwipeGesture (sender: UISwipeGestureRecognizer) {
-        
-    }
-    
-    @objc func handleRightSwipeGesture (sender: UISwipeGestureRecognizer) {
-        
-    }
-    
-    @objc func handleDownSwipeGesture (sender: UISwipeGestureRecognizer) {
     }
 }
