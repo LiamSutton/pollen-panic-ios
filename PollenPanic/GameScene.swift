791f2c4ed0677e5f42de8e7ef4d5204eb74dffb4
Liam Sutton <liamsutton1997@outlook.com>
2021-03-05 15:39:56 +0000

    tidyed up game scene code a bit

--- PollenPanic/GameScene.swift
+++ PollenPanic/GameScene.swift
@@ -9,7 +9,7 @@ import SpriteKit
 
 class GameScene : SKScene {
     
-    let bee = Bee()
+    var bee:Bee?
     var pollenCollection:PollenCollection?
     var pollutionCollection:PollutionCollection?
     var viewController:UIViewController?
@@ -23,6 +23,58 @@ class GameScene : SKScene {
     
     
     override func didMove(to view: SKView) {
+        
+        
+        setupGestures(view: view)
+        
+        gridSize = (Int(frame.width)-Constants.SPRITE_SIZE)/Constants.SPRITE_SIZE
+        
+        bee = Bee()
+        pollenCollection = PollenCollection(view: view, gridSize: gridSize!)
+        pollutionCollection = PollutionCollection(view: view, gridSize: gridSize!)
+        
+        setupBee()
+        setupPollenCollection()
+        setupPollutionCollection()
+        
+        
+        setupSfx()
+        setupBackground()
+        setupUI()
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        checkAllCollisions()
+        bee?.move()
+        pollenCollection!.move()
+        pollutionCollection!.move()
+        
+        for item in pollenCollection!.items {
+            if (item.position.y < 0) {
+                pollenCollection?.resetItemPosition(item: item)
+            }
+        }
+        
+        for item in pollutionCollection!.items {
+            if (item.position.y < 0) {
+                pollutionCollection?.resetItemPosition(item: item)
+            }
+        }
+    }
+    
+    func setupBee() {
+        bee?.size = CGSize(width: 64, height: 64)
+        bee?.position = CGPoint(x: size.width * 0.5, y: size.height * 0.1)
+        
+        let beeRange:SKRange = SKRange(lowerLimit: 32, upperLimit: frame.width-32)
+        let beeConstraint:SKConstraint = SKConstraint.positionX(beeRange)
+        
+        addChild(bee!)
+        
+        bee?.constraints = [beeConstraint]
+    }
+    
+    func setupGestures(view: SKView) {
         let leftSwipeGestureHandler = UISwipeGestureRecognizer(
             target: self, action: #selector(GameScene.handleLeftSwipeGesture(sender:))
         )
@@ -42,84 +94,6 @@ class GameScene : SKScene {
         view.addGestureRecognizer(leftSwipeGestureHandler)
         view.addGestureRecognizer(rightSwipeGestureHandler)
         view.addGestureRecognizer(downSwipeGestureHandler)
-        
-        backgroundColor = SKColor.systemTeal
-        gridSize = (Int(view.bounds.size.width)-Constants.SPRITE_SIZE)/Constants.SPRITE_SIZE
-        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
-        scoreLabel?.text = String(score)
-        scoreLabel?.fontSize = 46
-        scoreLabel?.fontColor = SKColor.white
-        scoreLabel?.position = CGPoint(x: frame.midX, y: view.bounds.height-150)
-        pollenCollection = PollenCollection(view: view, gridSize: gridSize!)
-        pollutionCollection = PollutionCollection(view: view, gridSize: gridSize!)
-        setupBee()
-        setupPollenCollection()
-        setupPollutionCollection()
-        addChild(scoreLabel!)
-        
-        if let backgroundMusicUrl = Bundle.main.url(forResource: Constants.BACKGROUND_MUSIC, withExtension: "mp3") {
-            backgroundMusic = SKAudioNode(url: backgroundMusicUrl)
-            addChild(backgroundMusic)
-        }
-        
-        if let pollenPickupSfxUrl = Bundle.main.url(forResource: Constants.POLLEN_PICKUP_SFX, withExtension: "mp3") {
-            pollenPickupSfx = SKAudioNode(url: pollenPickupSfxUrl)
-            pollenPickupSfx.autoplayLooped = false
-            addChild(pollenPickupSfx)
-        }
-        
-        if let gameOverSfxUrl = Bundle.main.url(forResource: Constants.GAME_OVER_SFX, withExtension: "mp3") {
-            gameOverSfx = SKAudioNode(url: gameOverSfxUrl)
-            gameOverSfx.autoplayLooped = false
-            addChild(gameOverSfx)
-        }
-        
-        let grassLeft = SKShapeNode(rectOf: CGSize(width: 128, height: view.bounds.size.height))
-        grassLeft.fillColor = SKColor.init(red: 0.2, green: 0.6, blue: 0.2, alpha: 1.0)
-        grassLeft.strokeColor = grassLeft.fillColor
-        grassLeft.lineWidth = 2
-        grassLeft.position = CGPoint(x: 32, y: frame.midY)
-        
-        let grassRight = SKShapeNode(rectOf: CGSize(width: 128, height: view.bounds.size.height))
-        grassRight.fillColor = grassLeft.fillColor
-        grassRight.strokeColor = grassLeft.strokeColor
-        grassRight.lineWidth = 2
-        grassRight.position = CGPoint(x: frame.width-32, y: frame.midY)
-        addChild(grassLeft)
-        addChild(grassRight)
-        
-    
-    }
-    
-    override func update(_ currentTime: TimeInterval) {
-        checkAllCollisions()
-        bee.move()
-        pollenCollection!.move()
-        pollutionCollection!.move()
-        
-        for item in pollenCollection!.items {
-            if (item.position.y < 0) {
-                pollenCollection?.resetItemPosition(item: item)
-            }
-        }
-        
-        for item in pollutionCollection!.items {
-            if (item.position.y < 0) {
-                pollutionCollection?.resetItemPosition(item: item)
-            }
-        }
-    }
-    
-    func setupBee() {
-        bee.size = CGSize(width: 64, height: 64)
-        bee.position = CGPoint(x: size.width * 0.5, y: size.height * 0.1)
-        
-        let beeRange:SKRange = SKRange(lowerLimit: 32, upperLimit: frame.width-32)
-        let beeConstraint:SKConstraint = SKConstraint.positionX(beeRange)
-        
-        addChild(bee)
-        
-        bee.constraints = [beeConstraint]
     }
     
     func setupPollenCollection() {
@@ -136,13 +110,22 @@ class GameScene : SKScene {
         }
     }
     
+    func setupUI() {
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel?.text = String(score)
+        scoreLabel?.fontSize = 46
+        scoreLabel?.fontColor = SKColor.white
+        scoreLabel?.position = CGPoint(x: frame.midX, y: frame.height-150)
+        addChild(scoreLabel!)
+    }
+    
     func checkForCollision(bee: GameObject, obstacle: GameObject) -> Bool {
         return bee.intersects(obstacle)
     }
     
     func checkAllCollisions() {
         for item in pollenCollection!.items {
-            let hasCollided:Bool = checkForCollision(bee: bee, obstacle: item)
+            let hasCollided:Bool = checkForCollision(bee: bee!, obstacle: item)
             if (hasCollided) {
                 pollenCollection?.resetItemPosition(item: item)
                 score += Constants.SCORE_INCREASE
@@ -152,7 +135,7 @@ class GameScene : SKScene {
         }
         
         for item in pollutionCollection!.items {
-            let hasCollided:Bool = checkForCollision(bee: bee, obstacle: item)
+            let hasCollided:Bool = checkForCollision(bee: bee!, obstacle: item)
             if (hasCollided) {
                 gameOverSfx.run(SKAction.play())
                 pollutionCollection?.resetItemPosition(item: item)
@@ -163,28 +146,61 @@ class GameScene : SKScene {
         }
     }
     
-    func drawBackground() {
+    func setupBackground() {
+        backgroundColor = SKColor.systemTeal
+        let grassLeft = SKShapeNode(rectOf: CGSize(width: 128, height: frame.height))
+        grassLeft.fillColor = SKColor.init(red: 0.2, green: 0.6, blue: 0.2, alpha: 1.0)
+        grassLeft.strokeColor = grassLeft.fillColor
+        grassLeft.lineWidth = 2
+        grassLeft.position = CGPoint(x: 32, y: frame.midY)
         
+        let grassRight = SKShapeNode(rectOf: CGSize(width: 128, height: frame.height))
+        grassRight.fillColor = grassLeft.fillColor
+        grassRight.strokeColor = grassLeft.strokeColor
+        grassRight.lineWidth = 2
+        grassRight.position = CGPoint(x: frame.width-32, y: frame.midY)
+        
+        addChild(grassLeft)
+        addChild(grassRight)
     }
-    // TODO: Fix memory leak, currently PISSING memory everywhere
+    
+    func setupSfx() {
+        if let backgroundMusicUrl = Bundle.main.url(forResource: Constants.BACKGROUND_MUSIC, withExtension: "mp3") {
+            backgroundMusic = SKAudioNode(url: backgroundMusicUrl)
+            addChild(backgroundMusic)
+        }
+        
+        if let pollenPickupSfxUrl = Bundle.main.url(forResource: Constants.POLLEN_PICKUP_SFX, withExtension: "mp3") {
+            pollenPickupSfx = SKAudioNode(url: pollenPickupSfxUrl)
+            pollenPickupSfx.autoplayLooped = false
+            addChild(pollenPickupSfx)
+        }
+        
+        if let gameOverSfxUrl = Bundle.main.url(forResource: Constants.GAME_OVER_SFX, withExtension: "mp3") {
+            gameOverSfx = SKAudioNode(url: gameOverSfxUrl)
+            gameOverSfx.autoplayLooped = false
+            addChild(gameOverSfx)
+        }
+    }
+  
+    
     @objc func gameOver() {
         viewController?.performSegue(withIdentifier: "GameToGameOver", sender: self.view)
         view?.presentScene(nil)
         viewController=nil
-        
-        
         view?.presentScene(nil)
     }
+    
     @objc func handleLeftSwipeGesture (sender: UISwipeGestureRecognizer) {
-        bee.changeDirection(newDirection: Constants.DIRECTION_LEFT)
+        bee?.changeDirection(newDirection: Constants.DIRECTION_LEFT)
     }
     
     @objc func handleRightSwipeGesture (sender: UISwipeGestureRecognizer) {
-        bee.changeDirection(newDirection: Constants.DIRECTION_RIGHT)
+        bee?.changeDirection(newDirection: Constants.DIRECTION_RIGHT)
     }
     
     @objc func handleDownSwipeGesture (sender: UISwipeGestureRecognizer) {
-        bee.changeDirection(newDirection: Constants.DIRECTION_NONE)
+        bee?.changeDirection(newDirection: Constants.DIRECTION_NONE)
     }
     
 }
